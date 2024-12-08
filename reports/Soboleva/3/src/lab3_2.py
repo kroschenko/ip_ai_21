129d80dd63972627d930dbc02b969c53af880d8e
Polina Soboleva <113055441+PoliNo4ka13@users.noreply.github.com>
2024-12-09 01:57:25 +0300

    laba3

---
+++ reports/Soboleva/3/src/lab3_2.py
@@ -0,0 +1,12 @@
+import os
+import torch
+print(torch.cuda.is_available())  
+
+YOLO_PATH = "yolov9"
+
+ 
+train_cmd = f"""
+python {YOLO_PATH}/train.py --img 1280 --batch 16 --epochs 50 \
+--data {YOLO_PATH}/data.yaml --weights yolov5n.pt --project yolov5_runs
+"""
+os.system(train_cmd)
