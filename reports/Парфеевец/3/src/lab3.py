a1bec0535bb2c1fd889c7e7a15bd0cc856679279
Andrew <khudzik24@mail.ru>
2024-12-12 12:54:39 +0300

    Add rep

--- reports/Парфеевец/3/src/lab3.py
+++ reports/Парфеевец/3/src/lab3.py
@@ -9,9 +9,9 @@ from models.common import DetectMultiBackend
 
 # Настройка модели
 YOLO_PATH = Path("")  # Путь к YOLOv5
-device = select_device('')  # Используем GPU, если доступно
-weights = "runs/train/papa/weights/best.pt"  # Путь к обученной модели
-data = str(YOLO_PATH / "data.yaml")
+device = select_device('mps')  # Используем GPU, если доступно
+weights = "/Users/andrewhudik/Downloads/ip_ai_21/reports/Hudik/lab_3/src/yolov5/yolov5m.pt"  # Путь к обученной модели
+data = str("/Users/andrewhudik/Downloads/ip_ai_21/reports/Парфеевец/3/src/data.yaml")
 
 # Загрузка модели
 model = DetectMultiBackend(weights, device=device, data=data)
@@ -62,5 +62,5 @@ def predict_video(source, output):
         vid_writer.release()
 
 # Запуск обработки видео
-predict_video("Брест день.mp4", "output_day.mp4")
-predict_video("Брест ночь.mp4", "output_night.mp4")
+predict_video('/Users/andrewhudik/Downloads/Brest Day Lab Work 3.mp4', "output_day.mp4")
+predict_video("/Users/andrewhudik/Downloads/Brest Night Lab Work 3.mp4", "output_night.mp4")
